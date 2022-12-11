01344155f9deef428aea6ed5e8d9892234f3271c
Open Vaet <openvaet@protonmail.com>
2022-12-11 02:43:47 +0100

    OpenVAET - v 1.32.24 - Pfizer Trials Study v 1.2

--- lib/OpenVaet/Controller/ReviewNejmFdaData.pm
+++ lib/OpenVaet/Controller/ReviewNejmFdaData.pm
@@ -490,4 +490,21 @@ sub load_efficacy_cases_week_by_week {
     );
 }
 
+sub study_changelog {
+    my $self = shift;
+    my $currentLanguage = $self->param('currentLanguage') // 'fr';
+
+    # Loggin session if unknown.
+    session::session_from_self($self);
+
+    my %languages = ();
+    $languages{'fr'} = 'French';
+    $languages{'en'} = 'English';
+
+    $self->render(
+        currentLanguage => $currentLanguage,
+        languages => \%languages
+    );
+}
+
 1;
